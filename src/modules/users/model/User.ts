e83020f7e2da9f3cf50a38f84e7a74ec3bd33970
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-20 16:15:30 -0300

    refactor(user): doesn't require data on constructor

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -8,22 +8,9 @@ class User {
   created_at: Date;
   updated_at?: Date;
 
-  constructor(
-    name: string,
-    admin: boolean,
-    email: string,
-    created_at: Date,
-    updated_at: Date
-  ) {
-    if (!this.id) {
-      this.id = uuidV4();
-    }
-
-    this.name = name;
-    this.admin = admin;
-    this.email = email;
-    this.created_at = created_at;
-    if (updated_at) this.updated_at = updated_at;
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+    if (!this.admin) this.admin = false;
   }
 }
 
