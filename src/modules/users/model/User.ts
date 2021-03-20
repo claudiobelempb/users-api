5ea461159e4eecb374c62918d47d5e483e425cac
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-20 15:05:02 -0300

    feat(userEntity)

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,30 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  updated_at?: Date;
+
+  constructor(
+    name: string,
+    admin: boolean,
+    email: string,
+    created_at: Date,
+    updated_at: Date
+  ) {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+
+    this.name = name;
+    this.admin = admin;
+    this.email = email;
+    this.created_at = created_at;
+    if (updated_at) this.updated_at = updated_at;
+  }
 }
 
 export { User };
