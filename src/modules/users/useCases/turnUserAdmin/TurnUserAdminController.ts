eff7e213eb1e8811f6b79e2fa1165c04977f8ef5
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-20 15:48:06 -0300

    feat(turnUserAdmin)

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,11 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    const user = this.turnUserAdminUseCase.execute({ user_id });
+
+    return response.json(user);
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,13 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("user doesn't exists");
+
+    this.usersRepository.turnAdmin(user);
+
+    return user;
   }
 }
 
