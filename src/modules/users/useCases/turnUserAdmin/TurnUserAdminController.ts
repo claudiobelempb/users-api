510ec56f01de656bba964ddb617fb76b90c76dba
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-20 16:17:17 -0300

    fix(turnUserAdmin): error treatment with try/catch

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,11 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
+    try {
+      const { user_id } = request.params;
 
-    const user = this.turnUserAdminUseCase.execute({ user_id });
+      const user = this.turnUserAdminUseCase.execute({ user_id });
 
-    return response.json(user);
+      return response.json(user);
+    } catch (error) {
+      return response.status(404).json({ error });
+    }
   }
 }
 
