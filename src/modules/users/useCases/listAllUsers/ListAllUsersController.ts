713d6e0ceed9f72d29fc4ec63b13d12df7fa8918
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-20 15:31:04 -0300

    feat(listAllUsers)

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    const allUsers = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.json(allUsers);
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,11 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (user.admin) return this.usersRepository.list();
+
+    throw new Error("user not admin");
   }
 }
 
