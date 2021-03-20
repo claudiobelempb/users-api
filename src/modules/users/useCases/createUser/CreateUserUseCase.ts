395e0a07d765052c4ccfc8c6ed0ec2439acc2ca3
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-20 15:21:43 -0300

    feat(createUser)

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const nameAlreadyInUse = this.usersRepository.findByEmail(email);
+
+    if (nameAlreadyInUse) throw new Error("email already in use");
+
+    const user = this.usersRepository.create({ email, name });
+
+    return user;
   }
 }
 
