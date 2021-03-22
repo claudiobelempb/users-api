783a89a551cd9a85897b7b7881363bf2e9a9a989
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-22 00:41:47 -0300

    feat(documentation): add routes documentation with Swagger-UI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
